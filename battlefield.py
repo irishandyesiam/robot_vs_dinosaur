06361b2f933c3abdaac5610cacb7b99891ff6a63
irishandyesiam <nathan.irish@gmail.com>
2022-08-14 21:48:54 -0500

    battle_phrase and display_winnner updated and working

--- battlefield.py
+++ battlefield.py
@@ -8,26 +8,30 @@ class Battlefield:
            
     def run_game(self):
         self.display_welcome()
-        robo_fighter.attack(dino_fighter) ##this should also be in battle_phrase##
-        print(dino_fighter.health) ##this should happen in battle_phrase##
+        self.battle_phrase()
+         ##this should happen in battle_phrase##
 
     def display_welcome(self):
-        print("Two may enter, one will leave!")
+        print("What happens when you force a dinosaur to defend itself against a mindless killing machine? Find out today at THE KILLING FIELD. Two may enter, one will leave!")
         
     def battle_phrase(self):
-        pass
-
+        robo_fighter.attack(dino_fighter) 
+        print(f"{robo_fighter.name} attacks {dino_fighter.name}")
+        print(f"{dino_fighter.name} is damaged and now health is at {dino_fighter.health}.")
+        dino_fighter.attack(robo_fighter)
+        print(f"{dino_fighter.name} attacks {robo_fighter.name}")
+        print(f"{robo_fighter.name} is damaged and now health is at {robo_fighter.health}.")
+        self.display_winner()
     def display_winner(self):
-        pass
-
+        if dino_fighter.health == 0:
+            print(f"{robo_fighter.name} is victorious!")
+        elif robo_fighter.health == 0:
+            print(f"{dino_fighter.name} is victorious!")
+        else:
+            self.battle_phrase()    
+    
 dino_fighter = Dinosaur("Earl Sinclair")
 robo_fighter = Robot("Kilroy")
 
-
-robo_fighter.attack(dino_fighter)
-print(dino_fighter.health)
-
-dino_fighter.attack(robo_fighter)
-print(robo_fighter.health)
 # print("find glory in 
 # death")
